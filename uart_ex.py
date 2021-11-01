8b95f7f8bebdb69f3cd1839307c878a89ecd2b97
speae <speae1232456@daum.net>
2021-11-01 19:20:25 +0900

    python3.6 serial protocol

--- uart_ex.py
+++ uart_ex.py
@@ -1,47 +1,106 @@
+import sys
+import os
+import threading
 import time
 import serial
-import threading 
+import signal 
 
-class ThreadWork(threading.Thread):
-    def __init__(self, name):
-        super.__init__()
-        self.name = name
+exitThread = False
+data = ''
 
-    def run(self):
-        print("UART Demonstration Program", threading.currentThread().getName())
+keyMap = {'a' : 'turn left',
+    'b' : 'turn right',
+    'c' : 'forward',
+    'd' : 'back',
+    'i' : 'stop'}
+
+
+def openSerial(port="/dev/ttyTHS1", baudrate=115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE,
+stopbits=serial.STOPBITS_ONE, timeout=None, xonxoff=False, rtscts=False, dsrdtr=False):
+
+    serial_port = serial.Serial(
+        port="/dev/ttyTHS1",
+        baudrate=115200,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE
+    )
+    
+    return serial_port
+
+def readThread(ser):
+    global exitThread
+    global data
+
+    while not exitThread:
+        data = ser.read()
+        print("output data : ", data)
+
+def writeThread(ser, data):
+    global exitThread
+
+    while not exitThread:
+        data = input("input command : \r\n")
+        try:
+            if keyMap[data]:
+                ser.write(data.encode("utf-8"))
+                print("data : ", data)
+            else:
+                exitThread = True
+        except Exception:
+            sys.exit()
+
+def readEOF(ser):
+    readed = ser.readline()
+    return readed[:-1]
+
+if __name__ == '__main__':
+
+    try:
+        ser = openSerial()
+
+        rx = threading.Thread(target=readThread, args=(ser,))
+        tx = threading.Thread(target=writeThread, args=(ser, data,))
+        
+        rx.start()
         time.sleep(1)
-        print("NVIDIA Jetson Nano Developer Kit", threading.currentThread().getName())
+        tx.start()
 
+        rx.join()
+        tx.join()
 
-serial_port = serial.Serial(
-    port="/dev/ttyTHS1",
-    baudrate=115200,
-    bytesize=serial.EIGHTBITS,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE
-)
+        print(readEOF(ser))
 
-time.sleep(1)
+    except KeyboardInterrupt:
+        exitThread = True
 
-try:
-    serial_port.write("UART Demonstration Program\r\n".encode())
-    serial_port.write("NVIDIA Jetson Nano Developer Kit\r\n".encode())
+# serial_port = serial.Serial(
+#     port="/dev/ttyTHS1",
+#     baudrate=115200,
+#     bytesize=serial.EIGHTBITS,
+#     parity=serial.PARITY_NONE,
+#     stopbits=serial.STOPBITS_ONE
+# )
 
-    while True:
-        if serial_port.inWaiting() > 0:
-            data = serial_port.read()
-            print(data)
+# try:
+#     serial_port.write("UART Demonstration Program\r\n".encode())
+#     serial_port.write("NVIDIA Jetson Nano Developer Kit\r\n".encode())
 
-            if data == "\r".encode():
-                serial_port.write("\n".encode())
+#     while True:
+#         if serial_port.inWaiting() > 0:
+#             data = serial_port.read()
+#             print(data)
 
-except KeyboardInterrupt:
-    print("Exist Program")
+#             if data == "\r".encode():
+#                 serial_port.write("\n".encode())
 
-except Exception as exception_error:
-    print("Error occured. Exiting Program")
-    print("Error : " + str(exception_error))
+# except KeyboardInterrupt:
+#     print("Exist Program")
 
-finally:
-    serial_port.close()
-    pass
+# except Exception as exception_error:
+#     print("Error occured. Exiting Program")
+#     print("Error : " + str(exception_error))
+
+# finally:
+#     serial_port.close()
+#     pass
